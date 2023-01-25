175d1585b224e1f49ae34d2f4e2757f3616eccc9
jgero <commit+github@jgero.me>
2023-01-25 18:44:48 +0100

    fix: formatting

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,16 +1,16 @@
 require('nvim-treesitter.configs').setup({
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false
-  },
-  incremental_selection = {
-    enable = true
-  },
-  -- context commetstring setup
-  -- source: https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    incremental_selection = {
+        enable = true,
+    },
+    -- context commetstring setup
+    -- source: https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+    },
 })
