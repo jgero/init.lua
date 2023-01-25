f2e11e4a1d50fcd28faa2e45d26f1eeb839e02fe
jgero <commit+github@jgero.me>
2023-01-25 22:32:43 +0100

    feat(lsp): add jdtls

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -17,7 +17,7 @@ local on_attach = function(client, bufnr)
     nmap('gd', vim.lsp.buf.definition, '[g]o to [d]efinition')
 end
 local capabilities =
-    require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspc = require('lspconfig')
 lspc.sumneko_lua.setup({
     capabilities = capabilities,
@@ -34,6 +34,10 @@ lspc.sumneko_lua.setup({
     },
 })
 lspc.rust_analyzer.setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+lspc.jdtls.setup({
     capabilities = capabilities,
     on_attach = on_attach,
 })
