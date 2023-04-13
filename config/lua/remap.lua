81f00921d7cfcd4f3c8edb725ddfd4cd42f5f69f
Johannes Gerold <git+commit@jgero.me>
2023-04-13 18:57:39 +0200

    fix(keybindings): change loading order of config files
    Currently the files are loaded in alphabetical order, which means some
    config files which set keybindings were loaded before the leader key was
    set, which lead to confusing behavior.

--- config/lua/remap.lua
+++ config/lua/remap.lua
@@ -1,14 +1,10 @@
 vim.keymap.set('n', '<leader>+', ':resize +1<cr>', { desc = 'grow current split' })
 vim.keymap.set('n', '<leader>-', ':resize -1<cr>', { desc = 'shrink current split' })
-vim.keymap.set('v', '<leader>y', '"+y', { desc = 'copy to clipboard' })
-vim.keymap.set('n', '<leader>y', '"+y', { desc = 'copy to clipboard' })
-vim.keymap.set('n', '<leader>p', '"+p', { desc = 'paste from clipboard' })
-vim.keymap.set('n', '<leader>P', '"+P', { desc = 'paste from clipboard' })
-vim.keymap.set('v', '<leader>p', '"+p', { desc = 'paste from clipboard' })
-vim.keymap.set('v', '<leader>P', '"+P', { desc = 'paste from clipboard' })
--- reset search highlight on enter press in normal mode
-vim.keymap.set('n', '<CR>', '{ -> v:hlsearch ? ":nohl\\<CR>" : "\\<CR>" }()', { expr = true })
+vim.keymap.set('', '<leader>y', '"+y', { desc = 'copy to clipboard' })
+vim.keymap.set('', '<leader>p', '"+p', { desc = 'paste from clipboard' })
+vim.keymap.set('', '<leader>P', '"+P', { desc = 'paste from clipboard' })
+vim.keymap.set('n', '<CR>', '{ -> v:hlsearch ? ":nohl\\<CR>" : "\\<CR>" }()', { expr = true, desc = 'reset search highlight' })
 -- exit terminal mode with escape
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'exit terminal mode with escape' })
 -- open new terminal on the bottom
-vim.keymap.set('', '<leader>t', ':split term://bash<cr>')
+vim.keymap.set('', '<leader>t', ':split term://zsh<cr>:startinsert<cr>', { desc = 'open terminal' })
