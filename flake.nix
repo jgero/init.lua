0dd28a56500fd93a292a3d88dd4ddd7d214b0ee1
Johannes Gerold <git+commit@jgero.me>
2023-06-29 00:26:27 +0200

    fix(formatting): fix treefmt and action with default stylua config

--- flake.nix
+++ flake.nix
@@ -19,13 +19,11 @@
       overlayFlakeInputs = prev: final: {
         neovim = neovim.packages.x86_64-linux.neovim;
       };
-
       overlayMyNeovim = prev: final: {
         myNeovim = import ./packages/myNeovim.nix {
           pkgs = final;
         };
       };
-
       pkgs = import nixpkgs {
         system = "x86_64-linux";
         overlays = [
@@ -34,15 +32,11 @@
           overlayMyNeovim
         ];
       };
+      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
     in
     {
-      formatter.x86_64-linux = treefmt-nix.lib.mkWrapper
-        nixpkgs.legacyPackages.x86_64-linux
-        {
-          projectRootFile = "flake.nix";
-          programs.nixpkgs-fmt.enable = true;
-          programs.stylua.enable = true;
-        };
+      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+      checks.x86_64-linux.formatter = treefmtEval.config.build.check self;
       packages.x86_64-linux.default = pkgs.myNeovim;
       apps.x86_64-linux.default = {
         type = "app";
